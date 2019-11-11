f8eac1bdebdf6fee54be31d63b9c018035e006fc
lde <ldemianiuk@gmail.com>
2019-11-11 16:09:57 +0100

    cleanup

--- tone/index.js
+++ tone/index.js
@@ -3,51 +3,33 @@ let conga_slap;
 let conga_open;
 let metronome;
 
+window.onload = setup;
+
 function onoff(synth, val) {
     if (val) synth.toMaster();
     else synth.disconnect();
 }
 
-function play() {
-    if (Tone.context.state !== 'running') {
-        Tone.context.resume();
-    }
-
+function setup() {
     Tone.Transport.bpm.value = 150;
 
-    clave = new Tone.Sampler({'C4': 'clave3.ogg'}, function() {
-        let pattern = new Tone.Sequence(function(time, note){
-            if (note) clave.triggerAttackRelease("C4", '4n', time);
-        }, [1, 0, 0, 1, 0, 0, 0, 1,
-            0, 0, 1, 0, 1, 0, 0, 0,], '8n');
-        Tone.Transport.start();
-        pattern.start(0);
-    }).toMaster();
+    clave = instrument('clave.ogg', 'C4',
+        ["C4", 0, 0, "C4", 0, 0, 0, "C4",
+            0, 0, "C4", 0, "C4", 0, 0, 0]);
 
-    conga_slap = new Tone.Sampler({'G3': 'conga slap.ogg'}, function() {
-        let pattern = new Tone.Sequence(function(time, note){
-            if (note) conga_slap.triggerAttackRelease("G3", '4n', time);
-        }, [0, 1, 0, 0], '4n');
-        Tone.Transport.start();
-        pattern.start(0);
-    }).toMaster();
-    conga_slap.volume.value = -3;
+    conga_slap = instrument('conga_slap.ogg', 'G3',
+        [0, 0, "C4", 0, 0, 0, 0, 0]);
 
-    conga_open = new Tone.Sampler({'C4': 'conga open.ogg'}, function() {
-        let pattern = new Tone.Sequence(function(time, note){
-            if (note) conga_open.triggerAttackRelease(note, '4n', time);
-        }, [0, 0, 0, 0, 0, 0, "C4", "C4",
-            0, 0, 0, "C3", "C3", 0, "C4", "C4"], '8n');
-        Tone.Transport.start();
-        pattern.start(0);
-    }).toMaster();
-    conga_open.volume.value = -10;
+    conga_open = instrument('conga_open.ogg', 'C4',
+        [0, 0, 0, 0, 0, 0, "C4", "C4",
+            0, 0, 0, "C3", "C3", 0, "C4", "C4"]);
+    conga_open.volume.value = -5;
 
-    metronome = new Tone.Sampler({'C4': 'clave.ogg'}, function() {
+    metronome = new Tone.Sampler({'C4': 'metronome.ogg'}, function() {
         let pattern2 = new Tone.Sequence(function(time, note){
             if (note) metronome.triggerAttackRelease(note, '256n', time);
-        }, ["C3", "C4"], '4n');
-        Tone.Transport.start();
+        }, ["C3", "C4", "C3", "C4"], '8n');
+
         pattern2.start(0);
     });
     metronome.volume.value = -15;
@@ -63,6 +45,35 @@ function play() {
     counting.start(0);
 }
 
+function play() {
+    if (Tone.context.state !== 'running') {
+        Tone.context.resume();
+    }
+
+    if(Tone.Transport.state == 'stopped') {
+        Tone.Transport.start();
+        document.getElementById('play').innerHTML = 'Stop';
+    }
+    else {
+        Tone.Transport.stop();
+        document.getElementById('play').innerHTML = 'Play';
+    }
+}
+
 function bpmChange (val) {
     Tone.Transport.bpm.value = val;
+}
+
+
+
+function instrument(sample, sampleNote, pattern) {
+    let instr = new Tone.Sampler({[sampleNote]: sample}, function() {
+        let tonePattern = new Tone.Sequence(function(time, note){
+            if (note) instr.triggerAttackRelease(note, '4n', time);
+            document.getElementById(sample.replace('.ogg', '_cb')).setAttribute("on", note !== 0);
+        }, pattern, '8n');
+
+        tonePattern.start(0);
+    }).toMaster();
+    return instr;
 }
